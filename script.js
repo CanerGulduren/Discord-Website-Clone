229c325dfd3dc8bb5af014d34a69890906609168
CanerGulduren <dngnhntr@gmail.com>
2023-03-26 17:38:55 +0300

    Scroll animation
    Make scrol animation

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ const menuIcon = document.querySelector(".menu-icon")
 const menu = document.querySelector(".menu")
 const cancelIcon = document.querySelector(".cancel-icon")
 
-menuIcon.addEventListener("click", ()=>{
-    menu.style.display = "block"
+window.addEventListener("click", (e)=>{
+    if(e.target === menuIcon){
+        menu.style.display = "flex"
+    }
+    else if(e.target === cancelIcon){
+        menu.style.display = "none"
+    }
+}
+)
+
+const observer = new IntersectionObserver((e) =>{
+    e.forEach((e)=>{
+        if(e.isIntersecting){
+            e.target.classList.add("show")
+        }
+    })
 })
 
-cancelIcon.addEventListener("click", ()=>{
-    menu.style.display = "none"
-})
+let hiddenElem = document.querySelectorAll(".hidden-elem")
+hiddenElem.forEach((e) => observer.observe(e))
